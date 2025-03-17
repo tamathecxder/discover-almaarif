c376dfde35075986030feb6053f6beb2f2fe6cbf
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-18 05:12:34 +0700

    add custom font

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,12 @@
   import Header from './lib/components/Header.svelte';
 </script>
 
-<Header />
+<div class="min-h-screen flex flex-col">
+  <Header />
+  
+  <main class="flex-1 container mx-auto p-4">
+    <h1 class="font-hanken-grotesk">Testing</h1>
+  </main>
 
-<main>
-
-</main>
-
-<Footer />
-
-<style></style>
+  <Footer />
+</div>

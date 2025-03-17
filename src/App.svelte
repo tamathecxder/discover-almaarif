68f2a34caf9803134e65ab9d2e94293c1f727cf0
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-18 05:51:35 +0700

    implement Header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-  import Footer from './lib/components/Footer.svelte';
-  import Header from './lib/components/Header.svelte';
+  import Footer from "./lib/components/Footer.svelte";
+  import Header from "./lib/components/Header.svelte";
+
+  const heroOverlay = "./assets/hero-overlay.png";
 </script>
 
-<div class="min-h-screen flex flex-col">
+<div class="relative min-h-[810px] bg-[url('{heroOverlay}')] bg-center bg-cover bg-no-repeat">
+  <div class="mix-blend-multiply absolute inset-0 bg-overlay/90 z-10"></div> <!-- Overlay -->
   <Header />
-  
-  <main class="flex-1 container mx-auto p-4">
-    <h1 class="font-hanken-grotesk">Testing</h1>
-  </main>
-
-  <Footer />
 </div>
+
+<main class="flex-1 container py-8 px-20">
+  <h1 class="font-hanken-grotesk">Testing</h1>
+</main>
+
+<Footer />
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    const brandLogo = 'assets/brand-logo.png';
+    const menuItems: string[] = ['Beranda', 'Blog', 'Kegiatan', 'Tentang', 'Kontak'];
+</script>
+
+<header class="container mx-auto z-20 absolute py-10 px-20 flex items-center justify-between">
+    <img src={brandLogo} alt="Brand Logo" class="h-10 object-center object-cover -mt-4 cursor-pointer">
+    <nav>
+        <ul class="flex gap-10 text-white">
+            {#each menuItems as item}
+                <li class="cursor-pointer hover:text-teal-200">
+                    {item}
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</header>

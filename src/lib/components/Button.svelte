53cf7696467e251a94d2ed6460b589f28bb70f72
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-19 01:16:19 +0700

    create Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let { 
+    type = "fill", 
+    text, 
+    icon = null } = $props();
+</script>
+
+<button
+  class="flex items-center gap-2 cursor-pointer rounded-md px-10 py-3 text-sm {type === 'fill'
+    ? 'bg-white text-black hover:opacity-90'
+    : 'border border-white hover:bg-overlay/100'}">
+    {#if icon}
+        <img src={icon} alt="Icon" class="w-4 h-4">
+    {/if}
+    <span>{text}</span>
+</button>

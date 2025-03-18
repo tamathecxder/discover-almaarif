51b42d297a6fa16645c3597f05348ff06df9684d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-19 01:55:02 +0700

    create Headline component

---
+++ src/lib/components/Headline.svelte
@@ -0,0 +1,8 @@
+<script>
+    let { type = 'default', category, desc } = $props(); 
+</script>
+
+<div class="flex flex-col items-center font-hanken-grotesk text-center">
+    <span class="uppercase text-lg font-bold mb-2 {type === 'default' ? 'text-primary' : 'text-white'}">{category}</span>
+    <h3 class="text-5xl max-w-2xl font-extrabold leading-tight {type === 'default' ? 'text-black' : 'text-white'}">{desc}</h3>
+</div>

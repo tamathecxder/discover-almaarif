c9fb968a58d38ec0a3a1b94c8f27b55dfc636496
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-20 05:49:53 +0700

    responsive UI on footer and header

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,92 @@
+<script>
+    const items = [
+        {
+            title: "Halaman",
+            children: ["Beranda", "Blog", "Kegiatan", "Tentang", "Kontak"],
+        },
+        {
+            title: "Kegiatan",
+            children: [
+                "Kajian Fiqih",
+                "Kajian Ibadah 4 Madzhab",
+                "Santunan Dhuafa",
+                "Pelatihan Kewirausahaan",
+                "Belajar Ngaji",
+            ],
+        },
+        {
+            title: "Kontak",
+            children: [
+                "081234567890 (WA)",
+                "987F1D6L (PIN BBM)",
+                "087634561234 (Telegram)",
+            ],
+        },
+    ];
+
+    const copyright = "© 2024 Masjid Al-Kandari All Rights Reserved";
+</script>
+
+<footer
+    class="flex flex-col justify-center items-center bg-primary text-beige pt-20 pb-10 px-10 lg:px-20 font-hanken-grotesk"
+>
+    <div
+        class="container flex flex-col lg:flex-row items-start justify-start gap-10 lg:gap-60 w-full"
+    >
+        <!-- Left -->
+        <div class="flex flex-col gap-6 items-start">
+            <img
+                src="assets/brand-logo.png"
+                alt="Brand Logo"
+                class="h-10 object-center object-cover"
+            />
+            <p class="max-w-xs text-sm">
+                Masjid ini memiliki desain arsitektur indah dan fasilitas
+                lengkap, menyediakan lingkungan yang nyaman untuk ibadah dan
+                kegiatan komunitas.
+            </p>
+
+            <div class="flex flex-row gap-4">
+                <div
+                    class="rounded-full bg-beige text-black w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-blue-200"
+                >
+                    <img src="assets/fb-outline.svg" alt="FB" class="w-4 h-4" />
+                </div>
+                <div
+                    class="rounded-full bg-beige text-black w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-blue-200"
+                >
+                    <img src="assets/ig-outline.svg" alt="FB" class="w-4 h-4" />
+                </div>
+                <div
+                    class="rounded-full bg-beige text-black w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-blue-200"
+                >
+                    <img
+                        src="assets/tiktok-outline.svg"
+                        alt="FB"
+                        class="w-4 h-4"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <!-- Right -->
+        <div class="flex flex-col lg:flex-row gap-10 lg:gap-[120px]">
+            {#each items as item}
+                <div class="flex flex-col gap-4">
+                    <h5 class="font-extrabold text-base">{item.title}</h5>
+                    <ul>
+                        {#each item.children as child}
+                            <li
+                                class="cursor-pointer text-sm font-normal mb-4 hover:text-blue-200"
+                            >
+                                {child}
+                            </li>
+                        {/each}
+                    </ul>
+                </div>
+            {/each}
+        </div>
+    </div>
+
+    <p class="mt-10 lg:mt-20 text-sm">{copyright}</p>
+</footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,30 +1,67 @@
 <script lang="ts">
     import Button from "./Button.svelte";
-    
-    const brandLogo = 'assets/brand-logo.png';
-    const menuItems: string[] = ['Beranda', 'Blog', 'Kegiatan', 'Tentang', 'Kontak'];
+    import Navbar from "./Navbar.svelte";
+
+    let isHamburgerOpen = $state(false);
+
+    function toggle() {
+        isHamburgerOpen = !isHamburgerOpen;
+        console.log("isHamburgerOpen", isHamburgerOpen);
+    }
+
+    const brandLogo = "assets/brand-logo.png";
+    const menuItems: string[] = [
+        "Beranda",
+        "Blog",
+        "Kegiatan",
+        "Tentang",
+        "Kontak",
+    ];
 </script>
 
-<header class="relative h-[810px] bg-[url('assets/hero-overlay.png')] bg-center bg-cover bg-no-repeat">
+<header
+    class="relative h-[810px] bg-[url('assets/hero-overlay.png')] bg-center bg-cover bg-no-repeat"
+>
     <div class="mix-blend-multiply absolute inset-0 bg-primary/90 z-10"></div>
-    
-    <div class="relative container mx-auto z-20 py-10 px-20 flex items-center justify-between">
-        <img src={brandLogo} alt="Brand Logo" class="h-10 object-center object-cover -mt-4 cursor-pointer">
-        <nav>
-            <ul class="flex gap-10 text-white">
-                {#each menuItems as item}
-                    <li class="cursor-pointer hover:text-teal-200">
-                        {item}
-                    </li>
-                {/each}
-            </ul>
-        </nav>
-    </div>
 
-    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container mx-auto z-20 py-10 px-20 flex flex-col items-center gap-8 text-white text-center">
-        <h1 class="text-6xl font-extrabold max-w-4xl tracking-wide">Temukan kedamaian melalui dekat dengan Tuhan</h1>
-        <p class="text-lg max-w-2xl">
-            Masjid bukan hanya sebagai tempat ibadah yang nyaman, tapi juga jadi tempat berbagai kegiatan, memupuk persaudaraan hingga membangkitkan perekonomian, atau tempat apapun yang berkaitan dengan keumatan.
+    <button
+        data-collapse-toggle="navbar-hamburger"
+        type="button"
+        class="lg:hidden bg-white fixed top-0 right-0 z-[9999] inline-flex items-center justify-center p-2 m-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-hamburger"
+        aria-expanded="false"
+        onclick={toggle}
+    >
+        <span class="sr-only">Open main menu</span>
+        <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+        >
+            <path
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+            />
+        </svg>
+    </button>
+
+    <Navbar {isHamburgerOpen} {brandLogo} {menuItems} />
+
+    <div
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container mx-auto z-20 py-10 px-10 lg:px-20 flex flex-col items-center gap-8 text-white text-center"
+    >
+        <h1 class="text-4xl lg:text-6xl font-extrabold max-w-4xl tracking-wide">
+            Temukan kedamaian melalui dekat dengan Tuhan
+        </h1>
+        <p class="text-sm lg:text-lg max-w-xl lg:max-w-2xl">
+            Masjid bukan hanya sebagai tempat ibadah yang nyaman, tapi juga jadi
+            tempat berbagai kegiatan, memupuk persaudaraan hingga membangkitkan
+            perekonomian, atau tempat apapun yang berkaitan dengan keumatan.
         </p>
 
         <div class="flex flex-col lg:flex-row gap-4">

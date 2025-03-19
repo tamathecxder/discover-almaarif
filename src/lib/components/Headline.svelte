46d26300248d6981c767caa7793cc7a8c99269fa
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-19 10:47:52 +0700

    update Headline style

--- src/lib/components/Headline.svelte
+++ src/lib/components/Headline.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex flex-col items-center font-hanken-grotesk text-center">
-    <span class="uppercase text-lg font-bold mb-2 {type === 'default' ? 'text-primary' : 'text-white'}">{category}</span>
-    <h3 class="text-5xl max-w-2xl font-extrabold leading-tight {type === 'default' ? 'text-black' : 'text-white'}">{desc}</h3>
+    <span class="uppercase text-lg font-bold mb-2 {type === 'default' ? 'text-primary' : 'text-beige'}">{category}</span>
+    <h3 class="text-5xl max-w-2xl font-extrabold leading-tight {type === 'default' ? 'text-black' : 'text-beige'}">{desc}</h3>
 </div>

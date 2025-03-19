33c014dd9d68312bf05d3b2802405deb920e9740
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-20 04:59:58 +0700

    impl responsive headline

--- src/lib/components/Headline.svelte
+++ src/lib/components/Headline.svelte
@@ -1,8 +1,19 @@
 <script>
-    let { type = 'default', category, desc } = $props(); 
+    let { type = "default", category, desc } = $props();
 </script>
 
 <div class="flex flex-col items-center font-hanken-grotesk text-center">
-    <span class="uppercase text-lg font-bold mb-2 {type === 'default' ? 'text-primary' : 'text-beige'}">{category}</span>
-    <h3 class="text-5xl max-w-2xl font-extrabold leading-tight {type === 'default' ? 'text-black' : 'text-beige'}">{desc}</h3>
+    <span
+        class="uppercase text-base lg:text-lg font-bold mb-2 {type === 'default'
+            ? 'text-primary'
+            : 'text-beige'}">{category}</span
+    >
+    <h3
+        class="text-3xl lg:text-5xl max-w-2xl font-extrabold leading-tight {type ===
+        'default'
+            ? 'text-black'
+            : 'text-beige'}"
+    >
+        {desc}
+    </h3>
 </div>

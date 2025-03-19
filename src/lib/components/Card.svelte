d973046854c26f74c55cb0b69183dd994aa95e99
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-03-19 10:47:40 +0700

    create card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { title, description, imgPath } = $props();
+</script>
+
+<div class="flex flex-col gap-4 px-6 py-4 bg-beige min-h-[420px] rounded-xl">
+    <div class="flex flex-col gap-4 max-w-72">
+        <h4 class="line-clamp-3 font-extrabold text-3xl">{title}</h4>
+        <p class="line-clamp-3 text-sm">{description}</p>
+    </div>
+
+    <img src={imgPath} alt={title} class="h-44 object-cover object-center rounded-lg">
+</div>
